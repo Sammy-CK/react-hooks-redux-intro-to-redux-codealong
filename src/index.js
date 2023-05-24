b67eed22e397449ac88695f78f2bbe8fdc80f425
Sammy-CK <sammycherono2696@gmail.com>
2023-05-24 12:11:16 +0300

    Added redux to app

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import ReactDOM from "react-dom";
 import counterReducer from "./features/counter/counterSlice.js";
 import App from "./App";
 import "./index.css";
+import { legacy_createStore as createStore} from 'redux'
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ); /* code change */
+  
+ReactDOM.render(    
+< Provider store={store}>
+<App />
+</ Provider >, document.getElementById("root"));
